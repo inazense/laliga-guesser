926af6c0db1fa9471976e0365668840f048e79ed
Inazio Claver <jclaver64@gmail.com>
2025-11-07 00:40:56 +0100

    Add spring boot initializr version

---
+++ src/test/java/com/inazense/laliga_analyzer/LaligaAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inazense.laliga_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaligaAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
